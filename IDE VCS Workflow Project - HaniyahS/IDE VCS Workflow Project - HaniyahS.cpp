b8297712a0dc4395585fb568127a8d74430c2b6f
Haniy <Haniy@HS>
2025-06-15 11:46:12 -0400

    Add project files.

---
+++ IDE VCS Workflow Project - HaniyahS/IDE VCS Workflow Project - HaniyahS.cpp
@@ -0,0 +1,16 @@
+// IDE VCS Workflow Project - HaniyahS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char first, middle, last;
+	first = 'H';
+	middle = 'A';
+	last = 'S';
+	cout << first << middle << last << endl;
+	return 0;
+}
+
+
