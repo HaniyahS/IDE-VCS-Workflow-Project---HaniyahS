e60e9f75e9b775e715d0e346928d8b732c6da8ef
Haniyah <Lionroar87453@gmail.com>
2025-06-15 13:31:29 -0400

    Pushing project-2 code

--- IDE VCS Workflow Project - HaniyahS/IDE VCS Workflow Project - HaniyahS.cpp
+++ IDE VCS Workflow Project - HaniyahS/IDE VCS Workflow Project - HaniyahS.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 int main()
 {
-	char first, middle, last;
-	first = 'H';
-	middle = 'A';
-	last = 'A';
-	cout << first << middle << last << endl;
+	string name, address, phonenumber;
+	name = "Haniyah Strong";
+	address = "123 Main Street";
+	phonenumber = "757-123-4567";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phonenumber << endl;
 	return 0;
 }
 
